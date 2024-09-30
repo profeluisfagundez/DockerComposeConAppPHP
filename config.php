8b611e2daccee6c555c87a329918f318089d16f7
Luis Eduardo Fagúndez <profeluisfagundez@gmail.com>
2024-09-30 15:02:22 -0300

    se genera un cambio en el config.php

--- config.php
+++ config.php
@@ -1,16 +1,17 @@
 <?php
-/*
 //Conexión para el contenedor
 $servername = "database"; // Cambiar a 'database' que es el nombre del servicio
 $username = "crudlite";    // Nombre de usuario definido en docker-compose
 $password = "crudlite";    // Contraseña definida en docker-compose
 $dbname = "crudlitesystem"; // Nombre de la base de datos definido en docker-compose
-*/
 
+/*
+//Conexion para trabajar local
 $servername = "localhost"; // Cambiar a 'database' que es el nombre del servicio
 $username = "root";    // Nombre de usuario definido en docker-compose
 $password = "root";    // Contraseña definida en docker-compose
 $dbname = "crudlitesystem"; // Nombre de la base de datos definido en docker-compose
+*/
 
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
